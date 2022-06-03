8a9158117bfb02bebb193ace158ad93ec8853e43
Rangsan AWB <rangsan.awb@gmail.com>
2022-06-03 12:32:10 +0700

    update event

--- app/src/main/java/com/wcc/citizenid/MainActivity.java
+++ app/src/main/java/com/wcc/citizenid/MainActivity.java
@@ -3,12 +3,26 @@ package com.wcc.citizenid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mResponseTextView;
+    private Button mPowerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPowerButton = findViewById(R.id.button_1);
+        mResponseTextView = findViewById(R.id.main_text_view_response);
     }
+
+    public void btn1Click(View v){
+        mResponseTextView.setText("Hello 000");
+    }
+
+
 }
