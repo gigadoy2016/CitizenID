aee1dccc93bdc2e0b478a616be5ac1fa2c18cdf1
Rangsan AWB <rangsan.awb@gmail.com>
2022-06-04 13:06:55 +0700

    04/jun

--- app/src/main/java/com/wcc/citizenid/MainActivity.java
+++ app/src/main/java/com/wcc/citizenid/MainActivity.java
@@ -1,27 +1,165 @@
 package com.wcc.citizenid;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.PendingIntent;
+import android.content.Context;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.acs.smartcard.Features;
+import com.acs.smartcard.PinModify;
+import com.acs.smartcard.PinVerify;
+import com.acs.smartcard.ReadKeyOption;
+import com.acs.smartcard.Reader;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
+    private static final String[] powerActionStrings = { "Power Down","Cold Reset", "Warm Reset" };
+    private static final String[] stateStrings = { "Unknown", "Absent","Present", "Swallowed", "Powered", "Negotiable", "Specific" };
+
+    private static final String[] featureStrings = { "FEATURE_UNKNOWN",
+            "FEATURE_VERIFY_PIN_START", "FEATURE_VERIFY_PIN_FINISH",
+            "FEATURE_MODIFY_PIN_START", "FEATURE_MODIFY_PIN_FINISH",
+            "FEATURE_GET_KEY_PRESSED", "FEATURE_VERIFY_PIN_DIRECT",
+            "FEATURE_MODIFY_PIN_DIRECT", "FEATURE_MCT_READER_DIRECT",
+            "FEATURE_MCT_UNIVERSAL", "FEATURE_IFD_PIN_PROPERTIES",
+            "FEATURE_ABORT", "FEATURE_SET_SPE_MESSAGE",
+            "FEATURE_VERIFY_PIN_DIRECT_APP_ID",
+            "FEATURE_MODIFY_PIN_DIRECT_APP_ID", "FEATURE_WRITE_DISPLAY",
+            "FEATURE_GET_KEY", "FEATURE_IFD_DISPLAY_PROPERTIES",
+            "FEATURE_GET_TLV_PROPERTIES", "FEATURE_CCID_ESC_COMMAND" };
+
+    private static final String[] propertyStrings = { "Unknown", "wLcdLayout",
+            "bEntryValidationCondition", "bTimeOut2", "wLcdMaxCharacters",
+            "wLcdMaxLines", "bMinPINSize", "bMaxPINSize", "sFirmwareID",
+            "bPPDUSupport", "dwMaxAPDUDataSize", "wIdVendor", "wIdProduct" };
+
+    private static final int DIALOG_VERIFY_PIN_ID = 0;
+    private static final int DIALOG_MODIFY_PIN_ID = 1;
+    private static final int DIALOG_READ_KEY_ID = 2;
+    private static final int DIALOG_DISPLAY_LCD_MESSAGE_ID = 3;
+
+    private static final int MAX_LINES = 25;
+    private UsbManager mManager;
+    private Reader mReader;
+    private PendingIntent mPermissionIntent;
+
+    /*---------------------------------------------------*/
     private TextView mResponseTextView;
+    private Spinner mReaderSpinner;
+    private ArrayAdapter<String> mReaderAdapter;
+    private Spinner mSlotSpinner;
+    private ArrayAdapter<String> mSlotAdapter;
+    private Spinner mPowerSpinner;
+    private Button mListButton;
+    private Button mOpenButton;
+    private Button mCloseButton;
+    private Button mGetStateButton;
     private Button mPowerButton;
+    private Button mGetAtrButton;
+    private CheckBox mT0CheckBox;
+    private CheckBox mT1CheckBox;
+    private Button mSetProtocolButton;
+    private Button mGetProtocolButton;
+    private EditText mCommandEditText;
+    private Button mTransmitButton;
+    private EditText mControlEditText;
+    private Button mControlButton;
+    private Button mGetFeaturesButton;
+    private Button mVerifyPinButton;
+    private Button mModifyPinButton;
+    private Button mReadKeyButton;
+    private Button mDisplayLcdMessageButton;
+
+    private Features mFeatures = new Features();
+    private PinVerify mPinVerify = new PinVerify();
+    private PinModify mPinModify = new PinModify();
+    private ReadKeyOption mReadKeyOption = new ReadKeyOption();
+    private String mLcdMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Get USB manager
+        mManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+
+        // Initialize reader
+        mReader = new Reader(mManager);
+
+        mReader.setOnStateChangeListener(new Reader.OnStateChangeListener() {
+            @Override
+            public void onStateChange(int slotNum, int prevState, int currState) {
+
+                if (prevState < Reader.CARD_UNKNOWN
+                        || prevState > Reader.CARD_SPECIFIC) {
+                    prevState = Reader.CARD_UNKNOWN;
+                }
+
+                if (currState < Reader.CARD_UNKNOWN
+                        || currState > Reader.CARD_SPECIFIC) {
+                    currState = Reader.CARD_UNKNOWN;
+                }
+
+                // Create output string
+                final String outputString = "Slot " + slotNum + ": "
+                        + stateStrings[prevState] + " -> "
+                        + stateStrings[currState];
+
+                // Show output
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        logMsg(outputString);
+                    }
+                });
+            }
+        });
+
+        //====================================================================
         mPowerButton = findViewById(R.id.button_1);
         mResponseTextView = findViewById(R.id.main_text_view_response);
     }
 
     public void btn1Click(View v){
         mResponseTextView.setText("Hello 000");
+    }
+
+    /**
+     * Logs the message.
+     *
+     * @param msg
+     *            the message.
+     */
+    private void logMsg(String msg) {
+
+        DateFormat dateFormat = new SimpleDateFormat("[dd-MM-yyyy HH:mm:ss]: ");
+        Date date = new Date();
+        String oldMsg = mResponseTextView.getText().toString();
+
+        mResponseTextView
+                .setText(oldMsg + "\n" + dateFormat.format(date) + msg);
+
+        if (mResponseTextView.getLineCount() > MAX_LINES) {
+            mResponseTextView.scrollTo(0,
+                    (mResponseTextView.getLineCount() - MAX_LINES)
+                            * mResponseTextView.getLineHeight());
+        }
     }
 
 
