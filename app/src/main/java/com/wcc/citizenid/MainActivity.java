b8f429ab5504d6cdadd522340e546485186c17b3
Rangsan AWB <rangsan.awb@gmail.com>
2022-06-10 19:04:05 +0700

    stat

--- app/src/main/java/com/wcc/citizenid/MainActivity.java
+++ app/src/main/java/com/wcc/citizenid/MainActivity.java
@@ -1,9 +1,15 @@
 package com.wcc.citizenid;
 
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -90,6 +96,124 @@ public class MainActivity extends AppCompatActivity {
     private ReadKeyOption mReadKeyOption = new ReadKeyOption();
     private String mLcdMessage;
 
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (ACTION_USB_PERMISSION.equals(action)) {
+                synchronized (this) {
+                    UsbDevice device = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+                    if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                        if (device != null) {
+                            // Open reader
+                            logMsg("Opening reader: " + device.getDeviceName() + "...");
+                            new OpenTask().execute(device);
+                        }
+                    } else {
+                        logMsg("Permission denied for device "+ device.getDeviceName());
+                        // Enable open button
+                        mOpenButton.setEnabled(true);
+                    }
+                }
+            } else if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action)) {
+                synchronized (this) {
+                    // Update reader list
+                    mReaderAdapter.clear();
+                    for (UsbDevice device : mManager.getDeviceList().values()) {
+                        if (mReader.isSupported(device)) {
+                            mReaderAdapter.add(device.getDeviceName());
+                        }
+                    }
+                    UsbDevice device = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+                    if (device != null && device.equals(mReader.getDevice())) {
+                        // Disable buttons
+                        mCloseButton.setEnabled(false);
+                        mSlotSpinner.setEnabled(false);
+//                        mGetStateButton.setEnabled(false);
+//                        mPowerSpinner.setEnabled(false);
+//                        mPowerButton.setEnabled(false);
+//                        mGetAtrButton.setEnabled(false);
+//                        mT0CheckBox.setEnabled(false);
+//                        mT1CheckBox.setEnabled(false);
+//                        mSetProtocolButton.setEnabled(false);
+//                        mGetProtocolButton.setEnabled(false);
+//                        mTransmitButton.setEnabled(false);
+//                        mControlButton.setEnabled(false);
+//                        mGetFeaturesButton.setEnabled(false);
+//                        mVerifyPinButton.setEnabled(false);
+//                        mModifyPinButton.setEnabled(false);
+//                        mReadKeyButton.setEnabled(false);
+//                        mDisplayLcdMessageButton.setEnabled(false);
+
+                        // Clear slot items
+                        //mSlotAdapter.clear();
+
+                        // Close reader
+                        logMsg("Closing reader...");
+                        new CloseTask().execute();
+                    }
+                }
+            }
+        }
+    };
+
+    private class OpenTask extends AsyncTask<UsbDevice, Void, Exception> {
+        @Override
+        protected Exception doInBackground(UsbDevice... params) {
+            Exception result = null;
+            try {
+                mReader.open(params[0]);
+            } catch (Exception e) {
+                result = e;
+            }
+            return result;
+        }
+        @Override
+        protected void onPostExecute(Exception result) {
+            if (result != null) {
+                logMsg(result.toString());
+            } else {
+                logMsg("Reader name: " + mReader.getReaderName());
+                int numSlots = mReader.getNumSlots();
+                logMsg("Number of slots: " + numSlots);
+                // Add slot items
+                mSlotAdapter.clear();
+                for (int i = 0; i < numSlots; i++) {
+                    mSlotAdapter.add(Integer.toString(i));
+                }
+                // Remove all control codes
+                mFeatures.clear();
+                // Enable buttons
+                mCloseButton.setEnabled(true);
+                mSlotSpinner.setEnabled(true);
+//                mGetStateButton.setEnabled(true);
+//                mPowerSpinner.setEnabled(true);
+//                mPowerButton.setEnabled(true);
+//                mGetAtrButton.setEnabled(true);
+//                mT0CheckBox.setEnabled(true);
+//                mT1CheckBox.setEnabled(true);
+//                mSetProtocolButton.setEnabled(true);
+//                mGetProtocolButton.setEnabled(true);
+//                mTransmitButton.setEnabled(true);
+//                mControlButton.setEnabled(true);
+//                mGetFeaturesButton.setEnabled(true);
+//                mReadKeyButton.setEnabled(true);
+//                mDisplayLcdMessageButton.setEnabled(true);
+            }
+        }
+    }
+    private class CloseTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            mReader.close();
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result) {
+            mOpenButton.setEnabled(true);
+        }
+    }
+
+    //--- init this Program
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -104,25 +228,16 @@ public class MainActivity extends AppCompatActivity {
         mReader.setOnStateChangeListener(new Reader.OnStateChangeListener() {
             @Override
             public void onStateChange(int slotNum, int prevState, int currState) {
-
-                if (prevState < Reader.CARD_UNKNOWN
-                        || prevState > Reader.CARD_SPECIFIC) {
+                if (prevState < Reader.CARD_UNKNOWN || prevState > Reader.CARD_SPECIFIC) {
                     prevState = Reader.CARD_UNKNOWN;
                 }
-
-                if (currState < Reader.CARD_UNKNOWN
-                        || currState > Reader.CARD_SPECIFIC) {
+                if (currState < Reader.CARD_UNKNOWN || currState > Reader.CARD_SPECIFIC) {
                     currState = Reader.CARD_UNKNOWN;
                 }
-
                 // Create output string
-                final String outputString = "Slot " + slotNum + ": "
-                        + stateStrings[prevState] + " -> "
-                        + stateStrings[currState];
-
+                final String outputString = "Slot " + slotNum + ": " + stateStrings[prevState] + " -> "  + stateStrings[currState];
                 // Show output
                 runOnUiThread(new Runnable() {
-
                     @Override
                     public void run() {
                         logMsg(outputString);
@@ -131,10 +246,141 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Register receiver for USB permission
+        mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_USB_PERMISSION);
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
+        registerReceiver(mReceiver, filter);
+
+        // Initialize response text view
+        mResponseTextView = findViewById(R.id.main_text_view_response);
+        mResponseTextView.setMovementMethod(new ScrollingMovementMethod());
+        mResponseTextView.setMaxLines(MAX_LINES);
+        mResponseTextView.setText("");
+
+        // Initialize reader spinner
+        mReaderAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item);
+        for (UsbDevice device : mManager.getDeviceList().values()) {
+            if (mReader.isSupported(device)) {
+                mReaderAdapter.add(device.getDeviceName());
+            }
+        }
+        mReaderSpinner = (Spinner) findViewById(R.id.main_spinner_reader);
+        mReaderSpinner.setAdapter(mReaderAdapter);
+
+        // Initialize slot spinner
+        mSlotAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item);
+        mSlotSpinner = (Spinner) findViewById(R.id.main_spinner_slot);
+        mSlotSpinner.setAdapter(mSlotAdapter);
+
         //====================================================================
         mPowerButton = findViewById(R.id.button_1);
-        mResponseTextView = findViewById(R.id.main_text_view_response);
+        // Initialize list button
+        mListButton = (Button) findViewById(R.id.main_button_list);
+        mListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mReaderAdapter.clear();
+                for (UsbDevice device : mManager.getDeviceList().values()) {
+                    if (mReader.isSupported(device)) {
+                        mReaderAdapter.add(device.getDeviceName());
+                    }
+                }
+            }
+        });
+
+        // Initialize open button
+        mOpenButton = (Button) findViewById(R.id.main_button_open);
+        mOpenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean requested = false;
+                // Disable open button
+                mOpenButton.setEnabled(false);
+                String deviceName = (String) mReaderSpinner.getSelectedItem();
+                if (deviceName != null) {
+                    // For each device
+                    for (UsbDevice device : mManager.getDeviceList().values()) {
+                        // If device name is found
+                        if (deviceName.equals(device.getDeviceName())) {
+                            // Request permission
+                            try{
+                                mManager.requestPermission(device,mPermissionIntent);
+                                //mResponseTextView.setText("Good");
+                            }catch (Exception e){
+                                mResponseTextView.setText("Err");
+                            }
+                            requested = true;
+                            break;
+                        }
+                    }
+                }
+                if (!requested) {
+                    // Enable open button
+                    mOpenButton.setEnabled(true);
+                }
+            }
+        });
+
+        // Initialize close button
+        mCloseButton = (Button) findViewById(R.id.main_button_close);
+        mCloseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Disable buttons
+                mCloseButton.setEnabled(false);
+                mSlotSpinner.setEnabled(false);
+//                mGetStateButton.setEnabled(false);
+//                mPowerSpinner.setEnabled(false);
+//                mPowerButton.setEnabled(false);
+//                mGetAtrButton.setEnabled(false);
+//                mT0CheckBox.setEnabled(false);
+//                mT1CheckBox.setEnabled(false);
+//                mSetProtocolButton.setEnabled(false);
+//                mGetProtocolButton.setEnabled(false);
+//                mTransmitButton.setEnabled(false);
+//                mControlButton.setEnabled(false);
+//                mGetFeaturesButton.setEnabled(false);
+//                mVerifyPinButton.setEnabled(false);
+//                mModifyPinButton.setEnabled(false);
+//                mReadKeyButton.setEnabled(false);
+//                mDisplayLcdMessageButton.setEnabled(false);
+//                // Clear slot items
+//                mSlotAdapter.clear();
+                // Close reader
+                logMsg("Closing reader...");
+                new CloseTask().execute();
+                mOpenButton.setEnabled(true);
+            }
+        });
+
+        // Initialize get state button
+        mGetStateButton = (Button) findViewById(R.id.main_button_get_state);
+        mGetStateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get slot number
+                int slotNum = mSlotSpinner.getSelectedItemPosition();
+                // If slot is selected
+                if (slotNum != Spinner.INVALID_POSITION) {
+                    try {
+                        // Get state
+                        logMsg("Slot " + slotNum + ": Getting state...");
+                        int state = mReader.getState(slotNum);
+                        if (state < Reader.CARD_UNKNOWN
+                                || state > Reader.CARD_SPECIFIC) {
+                            state = Reader.CARD_UNKNOWN;
+                        }
+                        logMsg("State: " + stateStrings[state]);
+                    } catch (IllegalArgumentException e) {
+                        logMsg(e.toString());
+                    }
+                }
+            }
+        });
     }
+
 
     public void btn1Click(View v){
         mResponseTextView.setText("Hello 000");
